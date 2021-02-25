433edf7e21e71651a43c95b164797a5c71ba5c34
Andre LaFleur <cincospenguinos@gmail.com>
2021-02-24 21:38:12 -0700

    Handle conversion

--- lib/give_me_speed.rb
+++ lib/give_me_speed.rb
@@ -13,13 +13,12 @@ module GiveMeSpeed
     def message
       return nil if speedcheck.enough_download? && speedcheck.enough_upload?
 
-      # TODO: Threshold management
       if !speedcheck.enough_download? && !speedcheck.enough_upload?
-        "I'm paying @comcast for 100 Mbps download and 100 Mbps upload but I'm only getting #{download_rate} and #{upload_rate}. What gives?"
+        "I'm paying @comcast for #{present_speed(speedcheck.thresholds[:download])} download and #{present_speed(speedcheck.thresholds[:upload])} upload but I'm only getting #{download_rate} and #{upload_rate}. What gives?"
       elsif !speedcheck.enough_upload?
-        "I'm paying @comcast for 100 Mbps upload but I'm only getting #{upload_rate}. What gives?"
+        "I'm paying @comcast for #{present_speed(speedcheck.thresholds[:upload])} upload but I'm only getting #{upload_rate}. What gives?"
       else
-        "I'm paying @comcast for 100 Mbps download but I'm only getting #{download_rate}. What gives?"
+        "I'm paying @comcast for #{present_speed(speedcheck.thresholds[:download])} download but I'm only getting #{download_rate}. What gives?"
       end
     end
 
@@ -31,6 +30,18 @@ module GiveMeSpeed
 
     def upload_rate
       speedcheck.last_run.pretty_upload_rate
+    end
+
+    def present_speed(rate)
+      units = %w(bps Kbps Mbps Gbps Tbps)
+      unit_index = 0
+
+      while rate > 1024
+        rate /= 1024
+        unit_index += 1
+      end
+
+      "#{rate} #{units[unit_index]}"
     end
   end
 
