8e0a9451f73feeeaac05f4b6a60bbe18255e4a2c
Andre LaFleur <cincospenguinos@gmail.com>
2021-02-24 21:40:32 -0700

    Extract class

---
+++ lib/give_me_speed/tweet_message.rb
@@ -0,0 +1,43 @@
+module GiveMeSpeed
+  class TweetMessage
+    attr_reader :speedcheck
+
+    def initialize(speedcheck)
+      @speedcheck = speedcheck
+    end
+
+    def message
+      return nil if speedcheck.enough_download? && speedcheck.enough_upload?
+
+      if !speedcheck.enough_download? && !speedcheck.enough_upload?
+        "I'm paying @comcast for #{present_speed(speedcheck.thresholds[:download])} download and #{present_speed(speedcheck.thresholds[:upload])} upload but I'm only getting #{download_rate} and #{upload_rate}. What gives?"
+      elsif !speedcheck.enough_upload?
+        "I'm paying @comcast for #{present_speed(speedcheck.thresholds[:upload])} upload but I'm only getting #{upload_rate}. What gives?"
+      else
+        "I'm paying @comcast for #{present_speed(speedcheck.thresholds[:download])} download but I'm only getting #{download_rate}. What gives?"
+      end
+    end
+
+    private
+
+    def download_rate
+      speedcheck.last_run.pretty_download_rate
+    end
+
+    def upload_rate
+      speedcheck.last_run.pretty_upload_rate
+    end
+
+    def present_speed(rate)
+      units = %w(bps Kbps Mbps Gbps Tbps)
+      unit_index = 0
+
+      while rate > 1024
+        rate /= 1024
+        unit_index += 1
+      end
+
+      "#{rate} #{units[unit_index]}"
+    end
+  end
+end
