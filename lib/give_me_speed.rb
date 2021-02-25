9bc527376d22595e085661ceb8f4dbf303c75d0e
Andre LaFleur <cincospenguinos@gmail.com>
2021-02-24 20:51:43 -0700

    No really I am starting over

--- lib/give_me_speed.rb
+++ lib/give_me_speed.rb
@@ -1,7 +1,5 @@
+require 'speedtest'
 require 'give_me_speed/version'
-require 'give_me_speed/threshold_check'
-require 'give_me_speed/speedtest_interface'
-require 'give_me_speed/message_builder'
 
 module GiveMeSpeed
   class Error < StandardError; end
@@ -9,37 +7,6 @@ module GiveMeSpeed
   class SpeedTest
     attr_reader :last_run
 
-    DEFAULT_CONFIG = {
-      download: 1,
-      upload: 1
-    }
-
     def initialize; end
-
-    def test
-      @last_run = SpeedTest.interface.test
-      ThresholdCheck.new(last_run, thresholds)
-    end
-
-    def self.config=(config = {})
-      @@config = DEFAULT_CONFIG.merge(config)
-    end
-
-    def self.interface
-      @@speedtest_interface ||= SpeedtestInterface.new
-    end
-
-    def self.interface=(interface)
-      @@speedtest_interface = interface
-    end
-
-    private
-
-    def thresholds
-      {
-        download: @@config[:download],
-        upload: @@config[:upload],
-      }
-    end
   end
 end
--- test/give_me_speed_test.rb
+++ test/give_me_speed_test.rb
@@ -1,25 +1,4 @@
 require 'test_helper'
 
 class GiveMeSpeedTest < Minitest::Test
-  def setup
-    GiveMeSpeed::SpeedTest.interface = mock_interface
-  end
-
-  def test_that_it_has_a_version_number
-    refute_nil ::GiveMeSpeed::VERSION
-  end
-
-  def test_speedtest_respects_provided_thresholds
-    GiveMeSpeed::SpeedTest.config = { download: 1000, upload: 1000 }
-    speedtest = GiveMeSpeed::SpeedTest.new
-    refute speedtest.test.enough_download?
-    refute speedtest.test.enough_upload?
-  end
-
-  private
-
-  def mock_interface
-    result = OpenStruct.new(download_rate: 100, upload_rate: 100)
-    OpenStruct.new(test: result, result: result)
-  end
 end
