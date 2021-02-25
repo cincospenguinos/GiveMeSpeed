e4f68cad58849a8b142b84e60d745503f7790b51
Andre LaFleur <cincospenguinos@gmail.com>
2021-02-24 21:07:58 -0700

    Setup speedcheck class

--- lib/give_me_speed.rb
+++ lib/give_me_speed.rb
@@ -2,11 +2,40 @@ require 'speedtest'
 require 'give_me_speed/version'
 
 module GiveMeSpeed
-  class Error < StandardError; end
-
-  class SpeedTest
-    attr_reader :last_run
-
+  class SpeedtestInterface
     def initialize; end
+
+    def run_test
+      Speedtest::Test.new.run
+    end
+  end
+
+  class SpeedCheck
+    attr_reader :thresholds
+
+    def initialize(thresholds)
+      @thresholds = thresholds
+      @last_run = nil
+    end
+
+    def enough_upload?
+      last_run.upload_rate >= thresholds[:upload]
+    end
+
+    def enough_download?
+      last_run.download_rate >= thresholds[:download]
+    end
+
+    def last_run
+      @last_run ||= SpeedCheck.interface.run_test
+    end
+
+    def self.interface
+      @@interface ||= SpeedtestInterface.new
+    end
+
+    def self.interface=(interface)
+      @@interface = interface
+    end
   end
 end
--- test/give_me_speed_test.rb
+++ test/give_me_speed_test.rb
@@ -1,4 +1,21 @@
 require 'test_helper'
 
 class GiveMeSpeedTest < Minitest::Test
+  def setup
+    GiveMeSpeed::SpeedCheck.interface = mock_interface
+  end
+
+  def test_speed_check_respects_threshold
+    check = GiveMeSpeed::SpeedCheck.new({ download: 100, upload: 1 })
+    assert check.enough_upload?
+    refute check.enough_download?
+  end
+
+  private
+
+  def mock_interface
+    result = OpenStruct.new(download_rate: 1, upload_rate: 1, pretty_download_rate: "1 Mbps",
+      pretty_upload_rate: "1 Mbps")
+    OpenStruct.new(run_test: result)
+  end
 end
